79fec90064632b1a7cb0d6749985eac03970a781
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-16 21:02:08 +0530

    Vite base changed for npm deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/CSA-Website-2026/',
+  base: '/CSA-Website-2026/',
   plugins: [react()],
 });
